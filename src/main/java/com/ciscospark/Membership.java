2c41f7064b384fbe6734e27a53969795f1f9472c
Michael Cervantes <mcervantes@tropo.com>
2015-11-25 16:10:50 -0800

    add new java client for Hydra.  Also add ability to order resources in RAML [COLLAB-126]

---
+++ src/main/java/com/ciscospark/Membership.java
@@ -0,0 +1,61 @@
+package com.ciscospark;
+
+/**
+ * Created on 11/24/15.
+ */
+public class Membership {
+    private String id;
+    private String roomId;
+    private String personId;
+    private String personEmail;
+    private Boolean isModerator;
+    private Boolean isMonitor;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    public Boolean getIsModerator() {
+        return isModerator;
+    }
+
+    public void setIsModerator(Boolean isModerator) {
+        this.isModerator = isModerator;
+    }
+
+    public Boolean getIsMonitor() {
+        return isMonitor;
+    }
+
+    public void setIsMonitor(Boolean isMonitor) {
+        this.isMonitor = isMonitor;
+    }
+}
