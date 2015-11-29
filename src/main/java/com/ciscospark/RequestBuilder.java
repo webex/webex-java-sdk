5eb86338a6ca7070ed3e891baf08e9a406abe54e
Michael Cervantes <mcervantes@tropo.com>
2015-11-29 02:52:03 -0800

    Revert "introduce parameterized test data for pagination [COLLAB-222] [COLLAB-224] [COLLAB-173] [COLLAB-144]"
    This reverts commit bea5567d095a6bbd19579197535f346582c066f8.

--- src/main/java/com/ciscospark/RequestBuilder.java
+++ src/main/java/com/ciscospark/RequestBuilder.java
@@ -1,8 +1,6 @@
 package com.ciscospark;
 
-import java.net.URL;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created on 11/24/15.
@@ -11,11 +9,9 @@ public interface RequestBuilder<T> {
     RequestBuilder<T> queryParam(String key, String value);
     RequestBuilder<T> path(String path);
     <NewType> RequestBuilder<NewType> path(String path, Class<NewType> clazz);
-    RequestBuilder<T> url(URL url);
     T post(T body);
     T put(T body);
     T get();
-    Iterator<T> iterate();
-    LinkedResponse<List<T>> paginate();
+    Iterator<T> list();
     void delete();
 }
