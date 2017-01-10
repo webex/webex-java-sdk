664b807b38784a6a43f7eeccf9a0d0b075190713
Santosh Kumar <santokum@cisco.com>
2017-01-10 18:20:06 +0000

    COLLAB-2206 added /people

--- src/main/java/com/ciscospark/Person.java
+++ src/main/java/com/ciscospark/Person.java
@@ -1,5 +1,7 @@
 package com.ciscospark;
 
+import java.util.Date;
+
 /**
  * Copyright (c) 2015 Cisco Systems, Inc. See LICENSE file.
  */
@@ -7,6 +9,17 @@ public class Person {
     private String id;
     private String displayName;
     private String[] emails;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private String orgId;
+    private String[] roles;
+    private String[] licenses;
+    private Date created;
+    private String timeZone;
+    private Date lastActivity;
+    private String status;
+    private String type;
 
     public String getId() {
         return id;
@@ -30,5 +43,93 @@ public class Person {
 
     public void setEmails(String[] emails) {
         this.emails = emails;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(String orgId) {
+        this.orgId = orgId;
+    }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+
+    public String[] getLicenses() {
+        return licenses;
+    }
+
+    public void setLicenses(String[] licenses) {
+        this.licenses = licenses;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public Date getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setLastActivity(Date lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
